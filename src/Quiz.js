b7de40dba73eff8d67c1fa7e95dd1da74a68816c
Yan Lu <luyanrock@gmail.com>
2018-11-21 21:21:07 -0800

    fix quiz position.

--- src/Quiz.js
+++ src/Quiz.js
@@ -5,7 +5,7 @@ let quizData = require('./quiz_data.json');
 class Quiz extends Component {
     constructor(props) {
         super(props);
-        this.state = {quiz_position: 1};
+        this.state = {quiz_position: 0};
     }
     render() {
         return <div className='QuizQuestion'>{quizData.quiz_questions[this.state.quiz_position].instruction_text}</div>;
